cf985e097d16034139363f115b6873c43f8c2bda
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-01-26 15:51:46 +0200

    Adding history to chat

--- chat_app/src/basic_chat/index.ts
+++ chat_app/src/basic_chat/index.ts
@@ -2,18 +2,26 @@ import OpenAI from 'openai';
 
 const openai = new OpenAI();
 
-process.stdin.addListener('data', async function (input) {
-    const userInput = input.toString().trim();
+const context:OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [{
+    role: 'system',
+    content: 'You are a helpful chatbot'
+}]
+
+async function createChatCompletion() {
     const response = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
-        messages: [{
-            role: 'system',
-            content: 'You are a helpful chatbot'
-        }, {
-            role: 'user',
-            content: userInput
-        }]
+        messages: context
     })
-    console.log(response.choices[0].message.content)
+    const responseMessage = response.choices[0].message;
+    context.push(responseMessage)
+    console.log(`${response.choices[0].message.role}: ${response.choices[0].message.content}`)
+}
 
+process.stdin.addListener('data', async function (input) {
+    const userInput = input.toString().trim();
+    context.push({
+        role: 'user',
+        content: userInput
+    })
+    await createChatCompletion();
 })
