07126879980582383c543a03113afcc5d644a472
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-01-26 17:14:27 +0200

    Implementing a token limit

--- chat_app/src/basic_chat/index.ts
+++ chat_app/src/basic_chat/index.ts
@@ -1,6 +1,10 @@
 import OpenAI from 'openai';
+import { encoding_for_model } from 'tiktoken';
 
 const openai = new OpenAI();
+const encoder = encoding_for_model('gpt-3.5-turbo');
+
+const MAX_TOKENS = 700;
 
 const context:OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [{
     role: 'system',
@@ -14,6 +18,11 @@ async function createChatCompletion() {
     })
     const responseMessage = response.choices[0].message;
     context.push(responseMessage)
+
+    if (response.usage && response.usage.total_tokens > MAX_TOKENS) {
+        deleteOlderMessages();
+    }
+
     console.log(`${response.choices[0].message.role}: ${response.choices[0].message.content}`)
 }
 
@@ -25,3 +34,34 @@ process.stdin.addListener('data', async function (input) {
     })
     await createChatCompletion();
 })
+
+function deleteOlderMessages(){
+    let contextLength = getContextLength();
+    while (contextLength > MAX_TOKENS) {
+        for(let i= 0; i < context.length; i++) {
+            const message = context[i];
+            if (message.role != 'system') {
+                context.splice(i, 1);
+                contextLength = getContextLength();
+                console.log('New context length: '+ contextLength);
+                break;
+            }
+        }
+    }
+}
+
+function getContextLength(){
+    let length = 0;
+    context.forEach((message) => {
+        if (typeof message.content == 'string') {
+            length += encoder.encode(message.content).length;
+        } else if (Array.isArray(message.content)) {
+            message.content.forEach((messageContent) => {
+                if (messageContent.type == 'text') {
+                    length += encoder.encode(messageContent.text).length;
+                }
+            })
+        }
+    })
+    return length
+}
