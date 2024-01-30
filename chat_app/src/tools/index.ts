a2895e01ed6fad26125fb49b1416bfa53df9fa07
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-01-30 11:02:16 +0200

    First tool call

--- chat_app/src/tools/index.ts
+++ chat_app/src/tools/index.ts
@@ -2,6 +2,10 @@ import OpenAI from "openai";
 
 const openAI = new OpenAI();
 
+function getTimeOfDay(){
+    return '5:45'
+}
+
 async function callOpenAIWithTools() {
     const context: OpenAI.Chat.ChatCompletionMessageParam[] = [
         {
@@ -17,9 +21,42 @@ async function callOpenAIWithTools() {
     // configure chat tools (first openAI call)
     const response = await openAI.chat.completions.create({
         model: 'gpt-3.5-turbo-0613',
+        messages: context,
+        tools: [
+            {
+                type: 'function',
+                function: {
+                    name: 'getTimeOfDay',
+                    description: 'Get the time of day'
+                }
+            }
+        ],
+        tool_choice: 'auto'// the engine will decide which tool to use
+    });
+    // decide if tool call is required  
+    const willInvokeFunction = response.choices[0].finish_reason == 'tool_calls'
+    const toolCall = response.choices[0].message.tool_calls![0]
+    
+    if (willInvokeFunction) {
+        const toolName = toolCall.function.name
+
+        if (toolName == 'getTimeOfDay') {
+            const toolResponse = getTimeOfDay();
+            context.push(response.choices[0].message);
+            context.push({
+                role: 'tool',
+                content: toolResponse,
+                tool_call_id: toolCall.id
+            })
+        }
+    }
+
+    const secondResponse = await openAI.chat.completions.create({
+        model: 'gpt-3.5-turbo-0613',
         messages: context
-    })    
-    console.log(response.choices[0].message.content)
+    })
+    console.log(secondResponse.choices[0].message.content)
+
 }
 
 callOpenAIWithTools();
