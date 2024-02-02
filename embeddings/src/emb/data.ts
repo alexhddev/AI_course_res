3c5bc310d4103a179d909e67c310528d1085c087
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-02-02 10:39:17 +0200

    Saving embeddings

--- embeddings/src/emb/data.ts
+++ embeddings/src/emb/data.ts
@@ -1,6 +1,13 @@
+import { readFileSync, writeFileSync } from 'fs';
 import OpenAI from 'openai'
+import { join } from 'path';
 
 const openai = new OpenAI();
+
+type DataWithEmbeddings = {
+    input: string,
+    embedding: number[]
+}
 
 async function generateEmbeddings(input: string| string[]){
     const response = await openai.embeddings.create({
@@ -11,4 +18,31 @@ async function generateEmbeddings(input: string| string[]){
     return response;
 }
 
-generateEmbeddings(['Cat is on the roof', 'Dog is on the car'])
+function loadJSONData<T>(fileName: string):T{
+    const path = join(__dirname, fileName);
+    const rawData = readFileSync(path);
+    return JSON.parse(rawData.toString());
+}
+
+function saveDataToJsonFile(data: any, fileName: string){
+    const dataString = JSON.stringify(data);
+    const dataBuffer = Buffer.from(dataString);
+    const path = join(__dirname, fileName);
+    writeFileSync(path, dataBuffer);
+    console.log(`saved data to ${fileName}`);
+}
+
+async function main(){
+    const data = loadJSONData<string[]>('data.json');
+    const embeddings = await generateEmbeddings(data);
+    const dataWithEmbeddings: DataWithEmbeddings[] = [];
+    for(let i = 0; i < data.length; i++){
+        dataWithEmbeddings.push({
+            input: data[i],
+            embedding: embeddings.data[i].embedding
+        })
+    }
+    saveDataToJsonFile(dataWithEmbeddings, 'dataWithEmbeddings.json')
+}
+
+main();
