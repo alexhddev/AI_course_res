5da46926e05e474fba8129ff57e4c1d0297308a8
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-02-06 12:07:32 +0200

    Embedding function

--- embeddings/src/chroma/Client.ts
+++ embeddings/src/chroma/Client.ts
@@ -1,24 +1,29 @@
-import { ChromaClient } from 'chromadb'
+import { ChromaClient, OpenAIEmbeddingFunction } from 'chromadb'
 
 const client = new ChromaClient({
     path: 'http://localhost:8000'
 })
 
+const embeddingFunction = new OpenAIEmbeddingFunction({
+    openai_api_key: process.env.OPENAI_API_KEY!,
+    openai_model: 'text-embedding-3-small'
+})
+
 async function main(){
     const response = await client.createCollection({
-        name: 'data-test'
+        name: 'data-test2'
     })
     console.log(response)
 }
 
 async function addData() {
     const collection = await client.getCollection({
-        name: 'data-test'
+        name: 'data-test2',
+        embeddingFunction: embeddingFunction
     });
     const result = await collection.add({
         ids: ['id1'],
         documents:['Here is my entry'],
-    //    embeddings: [[0.1, 0.2]]
     })
     console.log(result)
 }
