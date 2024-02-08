5c53f6fafc93c92c51858525dd72158693d09c2e
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-02-08 10:29:00 +0200

    Pinecone index operations

--- embeddings/src/pinecone/Index.ts
+++ embeddings/src/pinecone/Index.ts
@@ -4,7 +4,61 @@ const pc = new Pinecone({
   apiKey: process.env.PINECONE_KEY!
 });
 
-async function main(){
+// string value
+// embedding
+
+// metadata - more info
+
+type CoolType = {
+  coolness: number;
+  reference: string;
+}
+
+// namespace: partition vectors from an index into smaller groups. Make operations limited to one namespace
+async function createNamespace() {
+  const index = getIndex();
+  const namespace = index.namespace('cool-namespace')
+}
+
+function getIndex() {
+  const index = pc.index<CoolType>('cool-index')
+  return index;
+}
+
+async function listIndexes() {
+  const result = await pc.listIndexes();
+  console.log(result)
+}
+
+function generateNumberArray(length: number) {
+  return Array.from({ length }, () => Math.random());
+}
+
+async function upsertVectors() {
+  const embedding = generateNumberArray(1536);
+  const index = getIndex();
+
+  const upsertResult = await index.upsert([{
+    id: 'id-1',
+    values: embedding,
+    metadata: {
+      coolness: 3,
+      reference: 'abdc'
+    }
+  }])
+}
+
+async function queryVectors(){
+  const index = getIndex();
+  const result = await index.query({
+    id: 'id-1',
+    topK: 1,
+    includeMetadata: true
+  });
+  console.log(result)
+}
+
+async function createIndex() {
   await pc.createIndex({
     name: 'cool-index',
     dimension: 1536,
@@ -16,6 +70,10 @@ async function main(){
       }
     }
   })
+}
+
+async function main() {
+  await queryVectors()
 }
 
 main();
