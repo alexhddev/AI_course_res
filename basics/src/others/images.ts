52fc9cd414f6ff5637f469855eef0a43097b0266
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-02-29 11:30:04 +0200

    Generating images with DALL-E

---
+++ basics/src/others/images.ts
@@ -0,0 +1,48 @@
+import OpenAI from 'openai'
+import { writeFileSync } from 'fs'
+
+const openai = new OpenAI()
+
+async function generateFreeImage() {
+    const response = await openai.images.generate({
+        prompt: 'A photo of a cat on a mat',
+        model: 'dall-e-2',
+        style: 'vivid',
+        size: '256x256',
+        quality: 'standard',
+        n: 1
+    });
+    console.log(response)    
+}
+
+async function generateFreeLocalImage() {
+    const response = await openai.images.generate({
+        prompt: 'A photo of a cat on a mat',
+        model: 'dall-e-2',
+        style: 'vivid',
+        size: '256x256',
+        quality: 'standard',
+        n: 1,
+        response_format: 'b64_json'
+    });
+    const rawImage = response.data[0].b64_json;
+    if (rawImage) {
+        writeFileSync('catMat.png', Buffer.from(rawImage, 'base64'))
+    }  
+}
+
+async function generateAdvancedImage(){
+    const response = await openai.images.generate({
+        prompt: 'Photo of a city at night with skyscrapers',
+        model: 'dall-e-3',
+        quality: 'hd',
+        size: '1024x1024',
+        response_format: 'b64_json'
+    })
+    const rawImage = response.data[0].b64_json;
+    if (rawImage) {
+        writeFileSync('city.png', Buffer.from(rawImage, 'base64'))
+    }  
+}
+
+generateAdvancedImage()
