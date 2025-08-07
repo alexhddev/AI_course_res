5658425eaedd80bd03a0d9171333988e770489c8
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-08-07 08:52:21 +0300

    sdf

---
+++ embeddings/src/emb/project2/main.ts
@@ -0,0 +1,47 @@
+import { readFileSync, writeFileSync } from "fs";
+import OpenAI from "openai";
+import { join } from "path";
+
+const openai = new OpenAI();
+
+export type DataWithEmbeddings = {
+  input: string;
+  embedding: number[];
+};
+
+export async function generateEmbeddings(input: string | string[]) {
+  const response = await openai.embeddings.create({
+    input: input,
+    model: "text-embedding-3-small",
+  });
+  return response;
+}
+
+export function loadJSONData<T>(fileName: string): T {
+  const path = join(__dirname, fileName);
+  const rawData = readFileSync(path);
+  return JSON.parse(rawData.toString());
+}
+
+function saveDataToJsonFile(data: any, fileName: string) {
+  const dataString = JSON.stringify(data);
+  const dataBuffer = Buffer.from(dataString);
+  const path = join(__dirname, fileName);
+  writeFileSync(path, dataBuffer);
+  console.log(`saved data to ${fileName}`);
+}
+
+async function main() {
+  const data = loadJSONData<string[]>("movies.json");
+  const embeddings = await generateEmbeddings(data);
+  const dataWithEmbeddings: DataWithEmbeddings[] = [];
+  for (let i = 0; i < data.length; i++) {
+    dataWithEmbeddings.push({
+      input: data[i],
+      embedding: embeddings.data[i].embedding,
+    });
+  }
+  saveDataToJsonFile(dataWithEmbeddings, "dataWithEmbeddings2.json");
+}
+
+main();
